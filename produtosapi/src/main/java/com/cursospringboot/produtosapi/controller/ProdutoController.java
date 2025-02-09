1f4c7fc9aaa09c61ddc52f7d1167447f6711cc46
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-02-08 23:16:35 -0300

    Adicionando o ProdutoRepository

--- produtosapi/src/main/java/com/cursospringboot/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/cursospringboot/produtosapi/controller/ProdutoController.java
@@ -1,17 +1,33 @@
 package com.cursospringboot.produtosapi.controller;
 
+import com.cursospringboot.produtosapi.Repository.ProdutoRepository;
 import com.cursospringboot.produtosapi.model.Produto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
-    public void salvar(@RequestBody Produto produto){
+    public Produto salvar(@RequestBody Produto produto){
+
+        var id = UUID.randomUUID() .toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
         System.out.println("Produto recebido: " + produto);
+
+        return produto;
     }
 }
