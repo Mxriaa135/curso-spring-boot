e94aec0de8774e5682e04b40c37e603bc1d62ad0
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-02-11 23:49:38 -0300

    Criação do método findByNome

--- produtosapi/src/main/java/com/cursospringboot/produtosapi/Repository/ProdutoRepository.java
+++ produtosapi/src/main/java/com/cursospringboot/produtosapi/Repository/ProdutoRepository.java
@@ -3,6 +3,8 @@ package com.cursospringboot.produtosapi.Repository;
 import com.cursospringboot.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoRepository extends JpaRepository <Produto, String> {
+import java.util.List;
 
+public interface ProdutoRepository extends JpaRepository <Produto, String> {
+    List<Produto> findByNome(String nome);
 }
