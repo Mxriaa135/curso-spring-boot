d8ae3e71d571245cbe662b5f99f20a42995b4f08
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-02-14 16:25:31 -0300

    Initial commit

---
+++ Arquiteturaspring/src/test/java/com/curso_spring_boot/Arquiteturaspring/ArquiteturaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso_spring_boot.Arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
