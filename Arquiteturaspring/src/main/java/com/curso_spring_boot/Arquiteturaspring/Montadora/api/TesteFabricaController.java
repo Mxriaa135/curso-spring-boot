172b4cda7570f9ed70c6a518361fa2aeff2b48b6
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-02-16 23:48:51 -0300

    Criação do controller da api

---
+++ Arquiteturaspring/src/main/java/com/curso_spring_boot/Arquiteturaspring/Montadora/api/TesteFabricaController.java
@@ -0,0 +1,20 @@
+package com.curso_spring_boot.Arquiteturaspring.Montadora.api;
+
+import com.curso_spring_boot.Arquiteturaspring.Montadora.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteFabricaController {
+
+    @Autowired
+    public Motor motor;
+
+    @PostMapping
+    public CarroStatus ligarCarro(@RequestBody Chave chave){
+        var carro = new HondaHRV(motor);
+        return carro.darIgnicao();
+    }
+}
