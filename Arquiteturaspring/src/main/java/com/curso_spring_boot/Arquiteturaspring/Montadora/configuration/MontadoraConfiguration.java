ef91e425cf80d53d756e5ff8933e7bed9a1fb340
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-02-17 23:33:02 -0300

    Adição de mais 3 beans de Motor

--- Arquiteturaspring/src/main/java/com/curso_spring_boot/Arquiteturaspring/Montadora/configuration/MontadoraConfiguration.java
+++ Arquiteturaspring/src/main/java/com/curso_spring_boot/Arquiteturaspring/Montadora/configuration/MontadoraConfiguration.java
@@ -8,14 +8,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MontadoraConfiguration {
 
-    @Bean
-    public Motor motor(){
+    @Bean(name = "motorAspirado")
+    public Motor motorAspirado(){
         Motor motor = new Motor();
         motor.setCavalos(120);
         motor.setCilindros(4);
         motor.setTipoMotor(TipoMotor.ASPIRADO);
         motor.setModelo("XPTO-0");
         motor.setLitragem(2.0);
+        return motor;
+    }
+    @Bean(name = "motorTurbo")
+    public Motor motorTurbo(){
+        Motor motor = new Motor();
+        motor.setCavalos(320);
+        motor.setCilindros(6);
+        motor.setTipoMotor(TipoMotor.TURBO);
+        motor.setModelo("SS-L");
+        motor.setLitragem(2.5);
+        return motor;
+    }
+    @Bean(name = "motorEletrico")
+    public Motor motorEletrico(){
+        Motor motor = new Motor();
+        motor.setCavalos(580);
+        motor.setCilindros(0);
+        motor.setTipoMotor(TipoMotor.ELETRICO);
+        motor.setModelo("M-E");
+        motor.setLitragem(0.0);
         return motor;
     }
 
