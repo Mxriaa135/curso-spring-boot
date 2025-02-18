3fa3348b0f10cabb964aa285281c50368216c496
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-02-17 23:34:18 -0300

    Adição do RequestMapping e do Qualifier

--- Arquiteturaspring/src/main/java/com/curso_spring_boot/Arquiteturaspring/Montadora/api/TesteFabricaController.java
+++ Arquiteturaspring/src/main/java/com/curso_spring_boot/Arquiteturaspring/Montadora/api/TesteFabricaController.java
@@ -2,19 +2,23 @@ package com.curso_spring_boot.Arquiteturaspring.Montadora.api;
 
 import com.curso_spring_boot.Arquiteturaspring.Montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
 
     @Autowired
+    @Qualifier("motorAspirado")
     public Motor motor;
 
     @PostMapping
     public CarroStatus ligarCarro(@RequestBody Chave chave){
         var carro = new HondaHRV(motor);
-        return carro.darIgnicao();
+        return carro.darIgnicao(chave);
     }
 }
